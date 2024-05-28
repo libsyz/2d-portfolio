0058f5b7a2139421b78560d6bd3bf355f36e67f1
Miguel Jimenez <denkatalabeno@gmail.com>
2024-05-28 07:13:05 +0200

    added furniture bounds to the room

--- src/game_logic/interaction.js
+++ src/game_logic/interaction.js
@@ -4,47 +4,12 @@
 import { k } from './kaboomCtx';
 
 export const createInteraction = (map, obj) => { 
-    const mapInteractables = {
-        mailbox: () => { 
-            map.add([
-                    k.area({
-                        shape: new k.Rect(k.vec2(0), obj.width, obj.height)
-                    }),
-                    k.body({isStatic: true}),
-                    k.pos(obj.x, obj.y),
-                    obj.name
+        map.add([
+            k.area({
+                shape: new k.Rect(k.vec2(0), obj.width, obj.height)
+                }),
+                k.body({isStatic: true}),
+                k.pos(obj.x, obj.y),
+                obj.name
                 ])
-        },
-        house_door: () => {
-            map.add([
-                k.area({
-                    shape: new k.Rect(k.vec2(0), obj.width, obj.height)
-                }),
-                k.body({isStatic: true}),
-                k.pos(obj.x, obj.y),
-                obj.name
-            ])
-        },
-        psychology_diploma: () => {
-            map.add([
-                k.area({
-                    shape: new k.Rect(k.vec2(0), obj.width, obj.height)
-                }),
-                k.body({isStatic: true}),
-                k.pos(obj.x, obj.y),
-                obj.name
-            ])
         }
-
-        
-    }
-
-
-    mapInteractables[obj.name]();
-    setCollision(obj.name);
-}
-
-
-const setCollision = (objName) => { 
-
-}
