f6805d840b91ecf2bc1760704cccbe5cb557d716
Miguel Jimenez <denkatalabeno@gmail.com>
2025-01-18 08:50:36 +0100

    green bad guy hurts and dies after 3 hits

--- src/game_logic/baddie_green_demon.js
+++ src/game_logic/baddie_green_demon.js
@@ -10,19 +10,19 @@ k.loadSprite('baddie_green_demon', './src/assets/baddie_green_demon.png', {
         up: { from: 4, to: 7, loop: true, speed: 4 },
         left: { from: 8, to: 11, loop: true, speed: 4 },
         right: { from: 12, to: 15, loop: true, speed: 4 },
-        dead: 16
+        dead: 20
     }
 })
 
 const baddiePatrol = () => {
-
+    
     let baddieCounter = 0;
     let xVel = -20 + Math.random() * 40;
     let yVel = -20 + Math.random() * 40;
     
     return { 
         patrol() {
-            this.onUpdate( () => {
+            this.patrolEvent = this.onUpdate( () => {
                 if ( baddieCounter == seconds(2) ) {
                     baddieCounter = 0;
                     xVel = -20 + Math.random() * 40;
@@ -63,51 +63,27 @@ export const createBaddieGreenDemon = () => {
         k.body({isStatic: true}),
         'baddie_green_demon',
         baddiePatrol()
-//scale constrains bounds to the right place :-) 
     ]);
     
     baddieGreenDemon.on('death', () => {
-        baddieGreenDemon.destroy();
+        baddieGreenDemon.patrolEvent.cancel();
+        baddieGreenDemon.unuse('patrol');
+        baddieGreenDemon.move(0, 0);
+        baddieGreenDemon.play('dead');
+        k.wait(0.5, () => {
+            baddieGreenDemon.destroy();
+        })
     })
 
     baddieGreenDemon.patrol();
 
-    return baddieGreenDemon;
+    baddieGreenDemon.onCollide('arrow', (arrow) => {
+            baddieGreenDemon.hurt(1);
+            arrow.destroy();
+    })
 
+    return baddieGreenDemon;
 } 
 
     
-    // every two seconds, patrols on random direction
-    // k.onUpdate("baddie_green_demon", (baddie) => {
-
-    //     // there is a current animation playing
-    //     let currentAnim = 'idle';
-
-    //     if (baddieCounter < seconds(2)) {
-    //         baddieGreenDemon.play(currentAnim); 
-    //         baddieCounter += 1 
-    //     } else { 
-    //         baddieCounter = 0;
-    //         baddieGreenDemon.stop(currentAnim);
-    //         xVel = -20 + Math.random() * 40;
-    //         yVel = -20 + Math.random() * 40;
-
-    //         if (xVel > 0) {
-    //             currentAnim = 'right';
-    //         } else if (xVel < 0) {
-    //             currentAnim = 'left';
-    //         }
-    //     }
-
-
-    //     if (baddieCounter < 60 ) {
-    //         baddieGreenDemon.move(xVel, yVel);
-            
-    //     }
-	// })
-
-    // baddieGreenDemon.onCollide('arrow', (arrow) => {
-    //     baddieGreenDemon.hurt(1);
-    //     arrow.destroy();
-    // })
-
+  
