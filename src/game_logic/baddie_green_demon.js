fa5db289d9e6db262d62303de55edf7f6074743b
Miguel Jimenez <denkatalabeno@gmail.com>
2025-01-18 09:22:22 +0100

    stuck with flash feature

--- src/game_logic/baddie_green_demon.js
+++ src/game_logic/baddie_green_demon.js
@@ -14,6 +14,42 @@ k.loadSprite('baddie_green_demon', './src/assets/baddie_green_demon.png', {
     }
 })
 
+const componentFlash = (k) => {
+    const flash = (interval = 0.15) => {
+        let _loop = null;
+
+        return {
+            id: "flash",
+
+            get isFlashing() {
+                return _loop !== null;
+            },
+
+            flash(duration = Infinity) {
+                if (_loop) this.cancelFlash(false);
+
+                const end = time() + duration;
+
+                this.trigger('flashStart');
+
+                _loop = k.loop(interval, () => {
+                    if (time() >= end) return this.cancelFlash();
+                    this.hidden = !this.hidden;
+                })
+            },
+
+            cancelFlash(triggerEvent = true) {
+                if (!_loop) return;
+                _loop.cancel();
+                _loop = null;
+                this.hidden = false;
+                if (triggerEvent) this.trigger('flashEnd');
+            }
+        }
+    }
+    return { flash };
+}
+
 const baddiePatrol = () => {
     
     let baddieCounter = 0;
@@ -58,8 +94,10 @@ export const createBaddieGreenDemon = () => {
         },
         k.area(),
         k.anchor('center'),
+        k.color(),
         k.health(3),
         k.scale(scale),
+        componentFlash(),
         k.body({isStatic: true}),
         'baddie_green_demon',
         baddiePatrol()
@@ -79,8 +117,15 @@ export const createBaddieGreenDemon = () => {
 
     baddieGreenDemon.onCollide('arrow', (arrow) => {
             baddieGreenDemon.hurt(1);
+            baddieGreenDemon.patrolEvent.cancel();
+            baddieGreenDemon.flash(1000);
             arrow.destroy();
+            k.wait(1, () => {
+                baddieGreenDemon.patrol();
+            })
     })
+
+
 
     return baddieGreenDemon;
 } 
